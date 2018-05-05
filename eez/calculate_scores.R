4226c554fb6637a69b255d674dffbf1229769d1e
jules32 <jstewart@nceas.ucsb.edu>
2018-05-04 17:03:11 -0700

    in progress: tbx workshop

--- eez/calculate_scores.R
+++ eez/calculate_scores.R
@@ -1,132 +1,17 @@
+## calculate_scores.R
 
-# STEP 1: Be sure to pull ohiprep before running!!
+## This script calculates OHI scores with the `ohicore` package.
+## - configure_toolbox.r ensures your files are properly configured for `ohicore`.
+## - The `ohicore` function CalculateAll() calculates OHI scores.
 
-# STEP 2: Install the appropriate ohicore, if necessary:
-library(devtools)
-devtools::install_github("ohi-science/ohicore@master") # typicaly this version will be used
-#devtools::install_github("ohi-science/ohicore@dev") # used when testing new code in ohicore
-#devtools::install_github("ohi-science/ohicore@master_a2015") # used if assessment was done prior to 2016 and not updated
+## set working directory for all OHI calculations
+# setwd("~/github/sam/eez")
 
-# STEP 3: Set repository name
-setwd("eez")
+## run the configure_toolbox.r script to check configuration
+source("configure_toolbox.r")
 
+## calculate scenario scores
+scores <- ohicore::CalculateAll(conf, layers)
 
-# STEP 4: If changes are made to any of the files in eez_layers_meta_data, run these:
-# updates the layers_eez.csv file to be toolbox compatible
-
-
-source("../eez_layers_meta_data/layers_eez_script.R")
-
-source("../eez_layers_meta_data/update_targets_with_pre_res.R")
-
-
-# STEP 4: Revise relevant code below and run assessments!
-library(ohicore)
-library(zoo)
-library(stringr)
-library(readr)
-
-
-source('https://raw.githubusercontent.com/OHI-Science/ohiprep/master/src/R/common.R')
-
-
-
-## Read in the layers.csv file with paths to the data files
-g <- read.csv("layers.csv", stringsAsFactors = FALSE, na.strings='')
-
-# filters the data and determines whether the file is available
-    lyrs = g %>%
-      filter(ingest==T) %>%
-      mutate(
-        path_in        = file.path(dir, fn),
-        path_in_exists = file.exists(path_in),
-        filename = sprintf('%s.csv', layer),
-        path_out = sprintf('layers/%s.csv', layer)) %>%
-      select(
-        targets, layer, name, description,
-        fld_value=name_data_fld, units,
-        path_in, path_in_exists, filename, path_out) %>%
-          arrange(targets, layer)
-
-# confirms that all data layers are available based on file paths
-    if (nrow(filter(lyrs, !path_in_exists)) != 0){
-      message('The following layers paths do not exist:\n')
-      print(filter(lyrs, !path_in_exists) %>% select(layer, path_in), row.names=F)
-      stop('Data cannot be found - check file paths/names in layers.csv' )
-    }
-
-
-# copy layers into specific scenario / layers folder ## JULIE: may not need this for country-opportunities-training (this is just a check really, nothing should change)
-    for (j in 1:nrow(lyrs)){ # j=1
-      stopifnot(file.copy(lyrs$path_in[j], lyrs$path_out[j], overwrite=TRUE))
-    }
-
-# delete extraneous files
-    files_extra = setdiff(list.files('layers'), as.character(lyrs$filename))
-    unlink(sprintf('layers/%s', files_extra))
-
-# layers registry (this includes files that are ingest=FALSE)
-    lyrs_reg = lyrs %>%
-      select(
-        targets, layer, name, description,
-        fld_value, units, filename)
-    write.csv(lyrs_reg, 'layers.csv', row.names=FALSE, na='')
-
-
-# Run check on layers
-    conf   = Conf(sprintf('conf'))
-
-    CheckLayers(layers.csv = sprintf('layers.csv'),
-                layers.dir = sprintf('layers'),
-                flds_id    = conf$config$layers_id_fields)
-
-
-# calculate scores for each year scenario and save to a single csv file:
-
-  scenario_years <- c(2012:2017)
-
-  scores_all_years <- data.frame()
-
-  for (s_year in scenario_years){  # s_year=2017
-
-    conf   <-  Conf('conf')
-    layers <-  Layers(layers.csv = 'layers.csv', layers.dir = 'layers')
-    layers$data$scenario_year <-  s_year
-
-    # calculate scores
-    scores_sy <- CalculateAll(conf, layers) %>%
-      mutate(year = s_year)
-
-    scores_all_years <- rbind(scores_all_years, scores_sy)
-
-  }
-
-write.csv(scores_all_years, 'scores.csv', na='', row.names=FALSE)
-
-
-### Some methods for visualizing the data
-
-# JSL: won't
-# score_check(commit="previous", scenario_year=2017,
-#             file_name="eez2017_com_hb_correct", save_csv = TRUE, NA_compare = TRUE)
-
-# compare <- read.csv("../score_check/eez2016_np_fix_diff_data_2017-10-06.csv")
-# dplyr::filter(compare, is.na(old_score), !is.na(score))
-#
-# library(ggplot2)
-# ggplot(filter(compare, year==2016 & dimension=="score" & goal == "NP"), aes(old_score, score)) +
-#   geom_point() +
-#   geom_abline(slope=1, intercept=0) +
-#   theme_bw()
-#
-#
-# filter(compare, is.na(old_score) & !is.na(score))
-#
-# # looking within a goal:
-# scatterPlot(repo="ohi-global", scenario="eez2015", commit="previous", goal="CP", dim="pressures", fileSave="CP_pressure_eez2015")
-#
-# goalHistogram(scenario="eez2016", goal="AO", dim="status", fileSave="AO_need_eez2016")
-#
-# ##
-# read.csv("eez/scores.csv") %>%
-#   filter(goal %in% c("FIS", "MAR"), region_id == 188, dimension=="score")
+## save scores as scores.csv
+readr::write_csv(scores, 'scores.csv', na='')
